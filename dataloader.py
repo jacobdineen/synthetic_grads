3f6f21a06adc6f4e65ad8a676cf586a046dcc228
jdineen123 <jd5ed@virginia.edu>
2021-04-05 12:26:25 -0700

    function to return cleaned mnist dataset

---
+++ dataloader.py
@@ -0,0 +1,17 @@
+import numpy as np
+from tensorflow import keras
+
+
+def get_MNIST(num_classes=10):
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    # Make sure images have shape (28, 28, 1)
+    x_train = np.expand_dims(x_train, -1)
+    x_test = np.expand_dims(x_test, -1)
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    return (x_train, y_train, x_test, y_test)
